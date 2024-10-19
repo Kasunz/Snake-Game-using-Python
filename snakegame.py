f813bc956b15ff9d23b13b76e4672b9c6949a7e9
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-19 22:15:08 +0530

    Add  main function

--- snakegame.py
+++ snakegame.py
@@ -47,9 +47,19 @@ def massage_box(subject, content):
 
 
 def main():
-    pass
-
-
+    width = 600
+    height = 600
+    rows = 30
+    win = pygame.display.set_mode((width, height))
+    s = snake((255, 0, 0), (10, 10)) # Snake color -->  Red , position --> 10, 10 
+    flag = True
+    clock = pygame.time.Clock()
+    
+    while flag:
+        pygame.time.delay(50) # ms milliseconds
+        clock.tick(10)
+        redrawWindow(win)
+        
 rows = None
 w = None
 h = None
@@ -58,4 +68,3 @@ cube.rows = rows
 cube.w = w
 
 main()
-
