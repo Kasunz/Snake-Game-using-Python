bd3b4b3537464297bf5fe4e861e57faf60b58ba0
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-19 21:39:33 +0530

    Basic structure for snake game

--- snakegame.py
+++ snakegame.py
@@ -1,2 +1,61 @@
 """ Snake Game Using Python """
 
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,255,0)):
+        pass
+    
+    def move(self, dirnx, dirny):
+        pass
+    
+    def draw(self, surface, eyes=False):
+        pass
+    
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+    
+    def reset(self):
+        pass
+    
+    def addCube(self):
+        pass
+    
+def drawGrid(w, rows, surface):
+    pass
+
+def redrawWindow(surface):
+    pass
+
+def randomSnack(rows, items):
+    pass
+
+def massage_box(subject, content):
+    pass
+
+
+def main():
+    pass
+
+
+rows = None
+w = None
+h = None
+
+cube.rows = rows
+cube.w = w
+
+main()
+
